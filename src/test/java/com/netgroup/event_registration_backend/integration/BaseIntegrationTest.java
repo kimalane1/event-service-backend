d927868c76f4e3e01cc1d72a8ba271797696afac
aljonakend <aljonakend@gmail.com>
2025-10-30 11:22:28 +0200

    adjust IT config

--- src/test/java/com/netgroup/event_registration_backend/integration/BaseIntegrationTest.java
+++ src/test/java/com/netgroup/event_registration_backend/integration/BaseIntegrationTest.java
@@ -2,29 +2,35 @@ package com.netgroup.event_registration_backend.integration;
 
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class BaseIntegrationTest {
 
   @Container
-  static final PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:16-alpine")
+  static final PostgreSQLContainer<?> POSTGRES =
+      new PostgreSQLContainer<>("postgres:17-alpine")
           .withDatabaseName("testdb")
           .withUsername("test")
           .withPassword("test");
 
   @DynamicPropertySource
   static void overrideProps(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+    registry.add("spring.datasource.username", POSTGRES::getUsername);
+    registry.add("spring.datasource.password", POSTGRES::getPassword);
   }
+
+
 }
